138d0263ef83e350c0e4a6b93e4482174fd3f988
Ng Zhi An <ngzhian@gmail.com>
2016-10-17 12:00:27 +0800

    Move test cases

--- test.sh
+++ test.sh
@@ -1,8 +1,19 @@
-# ./main "testcases/proc" # fails
+# ./test.sh
+# to run parsing tests
+# ./test.sh -t
+# to run parsing tests and type checks
 
-TESTCASES=./testcases/*
+if [[ $1 -eq "-t" ]]
+then
+    TESTCASES=./testcases/typecheck/*
+    echo "Typechecking..."
+else
+    TESTCASES=./testcases/parsing/*
+    echo "Check parsing..."
+fi
+
 for f in $TESTCASES
 do
     # echo "$f: `./main $f`"
-    printf "%-30s `./main $f`\n" "$f"
+    printf "%-40s `./main $1 $f`\n" "$f"
 done
