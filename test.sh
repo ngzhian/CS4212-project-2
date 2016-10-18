51a42cb3b4790667a93f16abf761b83fac45b842
Ng Zhi An <ngzhian@gmail.com>
2016-10-18 23:17:16 +0800

    Move testcases into more structured folders

--- test.sh
+++ test.sh
@@ -5,15 +5,25 @@
 
 if [[ "$1" == "-t" ]]
 then
-    TESTCASES=./testcases/typecheck/*
+    PASS=./testcases/typecheck/pass/*
+    FAIL=./testcases/typecheck/fail/*
     echo "Typechecking..."
 else
-    TESTCASES=./testcases/parsing/*
+    PASS=./testcases/parsing/pass/*
+    FAIL=./testcases/parsing/fail/*
     echo "Check parsing..."
 fi
 
-for f in $TESTCASES
+# f=./testcases/typecheck/scope
+# printf "%-60s `./main $1 $f`\n" "$f"
+echo "These should pass..."
+for f in $PASS
 do
-    # echo "$f: `./main $f`"
+    printf "%-60s `./main $1 $f`\n" "$f"
+done
+echo ""
+echo "These should fail..."
+for f in $FAIL
+do
     printf "%-60s `./main $1 $f`\n" "$f"
 done
