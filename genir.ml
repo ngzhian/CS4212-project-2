cf43a832198ffb6f96b8d6db4bc4902a1a3b4ee6
Ng Zhi An <ngzhian@gmail.com>
2016-10-27 09:41:37 +0800

    Add more ir generation

--- genir.ml
+++ genir.ml
@@ -12,36 +12,94 @@ let freshName _ =  labelSupply := !labelSupply + 1;
    yields a tuple where first component represents the IRC and
    second component a variable name which is bound to the result *)
 let rec translateB exp = match exp with
-  | BConst true -> let x = freshName() in
-    ([IRC_Assign (x, IRC_IConst 1)], x)
-  | BConst false -> let x = freshName() in
-    ([IRC_Assign (x, IRC_IConst 0)], x)
-  | And (e1, e2) -> (* 
-                       e1.code;
-                       if !e1.result goto l1
-                       e2.code;
-                       x = e2.result;
-                       goto l2;
-                       l1:
-                         x = 0;             Booleans represented as integers
-                       l2:
-                         *)
-
-    let r1 = translateB e1 in
-    let r2 = translateB e2 in
+  | And (e1, e2) ->
+    let code1, place1 = translateB e1 in
+    let code2, place2 = translateB e2 in
     let x = freshName() in
     let l1 = freshLabel() in
     let l2 = freshLabel() in
-    ((fst r1)
-     @
-     (irc_ZeroJump (snd r1,l1))
-     @
-     (fst r2)
-     @
-     [IRC_Assign (x, IRC_Var (snd r2));
-      IRC_Goto l2 ]
+    (code1
+     @ (irc_ZeroJump (place1,l1))
+     @ code2
+     @ [IRC_Assign (x, IRC_Var place2);
+        IRC_Goto l2 ]
      @
      [IRC_Label l1;
       IRC_Assign (x, IRC_IConst 0);
       IRC_Label l2],
      x)
+  | Eq (e1, e2) ->
+    let code1, place1 = translateB e1 in
+    let code2, place2 = translateB e2 in
+    let x = freshName() in
+    ( code1
+      @ code2
+      @ [IRC_Assign (x, IRC_Eq (place1, place2))]
+    , x)
+  | Gt (e1, e2) ->
+    let code1, place1 = translateB e1 in
+    let code2, place2 = translateB e2 in
+    let x = freshName() in
+    ( code1
+      @ code2
+      @ [IRC_Assign (x, IRC_Gt (place1, place2))]
+    , x)
+  | Plus (e1, e2) ->
+    let code1, place1 = translateB e1 in
+    let code2, place2 = translateB e2 in
+    let x = freshName() in
+    ( code1
+      @ code2
+      @ [IRC_Assign (x, IRC_Plus (place1, place2))]
+    , x)
+  | Minus (e1, e2) ->
+    let code1, place1 = translateB e1 in
+    let code2, place2 = translateB e2 in
+    let x = freshName() in
+    ( code1
+      @ code2
+      @ [IRC_Assign (x, IRC_Minus (place1, place2))]
+    , x)
+  | Times (e1, e2) ->
+    let code1, place1 = translateB e1 in
+    let code2, place2 = translateB e2 in
+    let x = freshName() in
+    ( code1
+      @ code2
+      @ [IRC_Assign (x, IRC_Times (place1, place2))]
+    , x)
+  | Division (e1, e2) ->
+    let code1, place1 = translateB e1 in
+    let code2, place2 = translateB e2 in
+    let x = freshName() in
+    ( code1
+      @ code2
+      @ [IRC_Assign (x, IRC_Division (place1, place2))]
+    , x)
+  | Not e ->
+    let x = freshName() in
+    let code, place = translateB e in
+    ( code
+      @ [IRC_Assign (x, IRC_Not place)]
+    , x)
+  | IConst i -> let x = freshName() in
+    ([IRC_Assign (x, IRC_IConst i)], x)
+  | BConst true -> let x = freshName() in
+    ([IRC_Assign (x, IRC_IConst 1)], x)
+  | BConst false -> let x = freshName() in
+    ([IRC_Assign (x, IRC_IConst 0)], x)
+  | Var v -> let x = freshName() in
+    ([IRC_Assign (x, IRC_Var v)], x)
+  | FuncExp (f, ps) ->
+    let x = freshName() in
+    let n = List.length ps in
+    let t' = List.map translateB ps in
+    let places = List.map (fun t -> [IRC_Param (snd t)]) t' in
+    let codes = List.map fst t' in
+    let placesgen = List.fold_left (@) [] places in
+    let codesgen = List.fold_left (@) [] codes in
+    ( codesgen
+      @ placesgen
+      @ [IRC_Call (1, n)]
+      @ [IRC_Get x], x)
+  | RcvExp _ -> ([],"")
