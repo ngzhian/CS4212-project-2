8ab5d4258b6880b43d3b3e9c12114ef82eb135c0
Vishnu Prem <vishnu@vishnuprem.com>
2016-11-03 17:14:23 +0800

    prompt yes

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,7 +2,7 @@
 OPAM_DEPENDS="ocamlfind ounit re"
 	 
 
-sudo add-apt-repository ppa:avsm/ppa
+sudo add-apt-repository -y ppa:avsm/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 
