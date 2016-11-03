0cdbe20ec49c900deca8c107675a166c73d20126
Vishnu Prem <vishnu@vishnuprem.com>
2016-11-03 17:05:24 +0800

    clean

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,7 +1,7 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS="ocamlfind ounit re"
 	 
-echo "yes" | sudo add-apt-repository ppa:$ppa
+echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam11
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
