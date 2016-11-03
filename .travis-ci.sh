0e19f8bba4cb87376191b5176767df6dc2dc00b5
Vishnu Prem <vishnu@vishnuprem.com>
2016-11-03 17:11:21 +0800

    Install latest OCAML

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,12 +1,14 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS="ocamlfind ounit re"
 	 
-echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam11
+
+sudo add-apt-repository ppa:avsm/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
-export OPAMYES=1
+
 opam init 
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
+
 make
 make test
