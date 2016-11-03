815d6d7dbd21f782eb3cae1879b6e565a7e4b747
Vishnu Prem <vishnu@vishnuprem.com>
2016-11-03 17:18:38 +0800

    opam auto setup

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,12 +1,11 @@
-# Edit this for your own project dependencies
+# Add opam dependencies here
 OPAM_DEPENDS="ocamlfind ounit re"
-	 
 
 sudo add-apt-repository -y ppa:avsm/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 
-opam init 
+opam init -a
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
 
